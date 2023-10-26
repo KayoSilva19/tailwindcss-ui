8ee0d1c3c8f2aae1b0335bd1a1effb3456610609
Kayo Silva <Kayohenriquesilva19@gmail.com>
2023-10-26 11:32:29 -0300

    tailwind theme colors

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,50 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [theme, setTheme] = useState('dark')
+
+  function handleChangeTheme() {
+    const html = document.querySelector('html')
+
+    if (theme === 'dark') {
+      setTheme('light')
+      return html.classList.remove('dark')
+    }
+
+    setTheme('dark')
+    html.classList.add('dark')
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div
+      className="
+        h-screen bg-zinc-100 text-zinc-950 dark:bg-zinc-800 
+         dark:text-zinc-50 grid place-content-center
+    "
+    >
+      <h1 className="text-center text-3xl">Ready to start with Tailwind!</h1>
+      <button
+        className="
+          block w-fit mx-auto my-r px-4 py-2 rounded-lg
+          border-transparent hover:border-cyan-500 border-2 
+          transition-colors
+        "
+        onClick={() => setCount(count + 1)}
+      >
+        Count is {count}
+      </button>
+      <button
+        className="
+          block w-fit mx-auto my-r px-4 py-2 rounded-lg
+          border-transparent hover:border-cyan-500 border-2 
+          transition-colors
+        "
+        onClick={handleChangeTheme}
+      >
+        Theme: {theme}
+      </button>
+    </div>
   )
 }
 
